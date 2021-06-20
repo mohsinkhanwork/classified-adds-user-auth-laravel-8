fba834b270210e307e0e9711441f8d024f68c665
mohsin khan <mkhan9658@gmail.com>
2021-06-21 00:45:19 +0500

    user auth

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+     <script src="{{ asset('js/app.js') }}" defer></script>
+   
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('js/bootstrap.js') }}" rel="stylesheet">
+    
+</head>
+<body>
+    
+
+
+
+
+
+
+
+
+
+
+    
+</body>
+</html>
